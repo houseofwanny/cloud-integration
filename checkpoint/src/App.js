2b4221e5201fbbeb1be3c5a8fda95b88cc61ffbc
houseofwanny <112391506+houseofwanny@users.noreply.github.com>
2023-03-06 12:44:58 +0100

    Commit

---
+++ checkpoint/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+    <div class="mapouter">
+<div class="gmap_canvas">
+  <h3>GPS CHECKPOINT</h3>
+    <iframe width="500" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=Nabeul&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+    </iframe>
+       <a href="https://www.whatismyip-address.com">
+       </a>
+       <a href="https://www.embedgooglemap.net">GPS</a>
+       </div>
+       </div>
+       </div>
+  );
+}
+
+export default App;
+
